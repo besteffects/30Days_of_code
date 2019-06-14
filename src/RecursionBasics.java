9c88b8c1a2e0b8b83eeeeab9f43b532a5fc444b9
Vitalii <besteffects@gmail.com>
2019-06-14 19:33:51 -0400

    RecursionBasics_Factorial

--- src/RecursionBasics.java
+++ src/RecursionBasics.java
@@ -3,6 +3,7 @@ public class RecursionBasics {
     public static void main(String[] args) {
         System.out.println(exponentiation(5, 3));
         System.out.println(summation(3));
+        System.out.println(factorial(3));
     }
 
     public static int exponentiation(int n, int p) {
@@ -31,6 +32,21 @@ public class RecursionBasics {
             //3+2+1+summation(0)
             //3+2+1+0=6
             return n + summation(n - 1);
+        }
+    }
+
+    public static int factorial(int n){
+        //base case
+        if (n<=1){
+            return 1;
+        }
+        //Recursive case
+        else{
+            //Factorial(4)=4*Factorial(3)
+            //4*3*Factorial(2)
+            //4*3*2*Factorial(1)
+            //4*3*2*1
+            return n*factorial(n-1);
         }
     }
 }
