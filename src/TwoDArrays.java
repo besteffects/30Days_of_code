fa6de26cc0d1a6ccb591161096226b6ede14eb98
Vitalii <besteffects@gmail.com>
2019-06-18 23:46:13 -0400

    11_2dArrays. improvement

--- src/TwoDArrays.java
+++ src/TwoDArrays.java
@@ -7,21 +7,74 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 public class TwoDArrays {
-    private static final Scanner scanner = new Scanner(System.in);
+
 
     public static void main(String[] args) {
         int[][] arr = new int[6][6];
 
-        for (int i = 0; i < 6; i++) {
-            String[] arrRowItems = scanner.nextLine().split(" ");
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        arr[0][0] = 1;
+        arr[0][1] = 1;
+        arr[0][2] = 1;
+        arr[0][3] = 0;
+        arr[0][4] = 0;
+        arr[0][5] = 0;
 
-            for (int j = 0; j < 6; j++) {
-                int arrItem = Integer.parseInt(arrRowItems[j]);
-                arr[i][j] = arrItem;
+        arr[1][0] = 0;
+        arr[1][1] = 1;
+        arr[1][2] = 0;
+        arr[1][3] = 0;
+        arr[1][4] = 0;
+        arr[1][5] = 0;
+
+        arr[2][0] = 1;
+        arr[2][1] = 1;
+        arr[2][2] = 1;
+        arr[2][3] = 0;
+        arr[2][4] = 0;
+        arr[2][5] = 0;
+
+        arr[3][0] = 0;
+        arr[3][1] = 0;
+        arr[3][2] = 2;
+        arr[3][3] = 4;
+        arr[3][4] = 4;
+        arr[3][5] = 0;
+
+        arr[4][0] = 0;
+        arr[4][1] = 0;
+        arr[4][2] = 0;
+        arr[4][3] = 2;
+        arr[4][4] = 0;
+        arr[4][5] = 0;
+
+        arr[5][0] = 0;
+        arr[5][1] = 0;
+        arr[5][2] = 1;
+        arr[5][3] = 2;
+        arr[5][4] = 4;
+        arr[5][5] = 0;
+
+        int sum[] = new int[16];
+        int hourglass = 0;
+        for (int i = 0; i < 4; i++) {
+//print the row of space-separated values
+            for (int j = 0; j < 4; j++) {
+                sum[hourglass] = ( arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2] );
+                hourglass++;
             }
         }
-
-        scanner.close();
+        //to print inner array use
+//        for (int j = 0; j < sum.length; j++) {
+//            System.out.print(sum[j] + " ");
+        int max = sum[0];
+        for (int counter = 0; counter < sum.length; counter++) {
+            if (sum[counter] > max) {
+                sum = arr[counter];
+            }
+        }
+        System.out.println("The highest maximum is: " + max);
     }
 }
+
+
+
--- src/TwoDArraysExample.java
+++ src/TwoDArraysExample.java
@@ -3,7 +3,10 @@ public class TwoDArraysExample {
         int rowSize = 2;
         int colSize = 4;
         int[][] myArray1 = new int[rowSize][colSize];
-
+       /* The outer loop represents the matrix's rows and uses as i its variable, and the inner loop
+       represents the matrix's columns and uses j as its variable. The code below assigns the value
+       of to each element in the 2D array we declared previously:
+        */
         int count = 0;
         for(int i = 0; i < rowSize; i++) {
             for(int j = 0; j < colSize; j++, count++) {
@@ -11,7 +14,6 @@ public class TwoDArraysExample {
             }
         }
         for(int i = 0; i < rowSize; i++) {
-
             // print the row of space-separated values
             for(int j = 0; j < colSize; j++) {
                 System.out.print(myArray1[i][j] + " ");
