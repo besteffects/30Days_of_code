aad7b54e76ec5524a2d957b414bd2b5975eea32e
Vitalii <besteffects@gmail.com>
2019-06-21 01:42:33 -0400

    Added animal package (inheritance)

---
+++ src/Animal/Cat.java
@@ -0,0 +1,15 @@
+package Animal;
+
+public class Cat extends Animal {
+    public Cat(){
+        super(7);
+        System.out.println("A cat has been created");
+    }
+
+    public void meow(){
+        System.out.println("A cat meows");
+    }
+    public void prence(){
+        System.out.println("A cat is prancing");
+    }
+}
