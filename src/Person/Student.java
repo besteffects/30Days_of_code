e858cb7f1273c249b3c5859ab69c7a4eb497d982
Vitalii <besteffects@gmail.com>
2019-06-21 00:16:17 -0400

    12_Inheritance. Finilized task

--- src/Person/Student.java
+++ src/Person/Student.java
@@ -1,10 +1,8 @@
 package Person;
 
-public class Student extends Person{
+ class Student extends Person {
     private int[] testScores;
-    private String grade;
-    private int average;
-    private int total;
+
     /*
      *  Class Constructor
      *
@@ -13,27 +11,43 @@ public class Student extends Person{
      *   @param id - An integer denoting the Person's ID number.
      *   @param scores - An array of integers denoting the Person's test scores.*/
     // Write your constructor here
-    public Student(String firstName, String lastName, int identification, int [] scores) {
+     Student(String firstName, String lastName, int identification, int[] scores) {
         super(firstName, lastName, identification);
-        this.testScores=testScores;
+        this.testScores = scores;
     }
     /*
      *   Method Name: calculate
      *   @return A character denoting the grade.
      */
-
-    public int calculate() {
-        total=0;
-        for (int i = 0; i < testScores.length; i++) {
-            total=total+testScores[i];
-        }
-        average=total/testScores.length;
-        return average;
-    }
-    public void printPerson(){
-    }
-
-
     // Write your method here
 
+     char calculate() {
+        String grade = "";
+        int total = 0;
+        int avg;
+        for (int i = 0; i < testScores.length; i++) {
+            total = total + testScores[i];
+        }
+        avg = total / testScores.length;
+
+        if (avg < 40) {
+            grade = "T";
+        }
+        if (avg >= 40 && avg < 55) {
+            grade = "D";
+        }
+        if (avg > 55 && avg < 70) {
+            grade = "P";
+        }
+        if (avg >= 70 && avg < 80) {
+            grade = "A";
+        }
+        if (avg >= 80 && avg < 90) {
+            grade = "E";
+        }
+        if (avg >= 90 && avg <= 100) {
+            grade = "O";
+        }
+        return grade.charAt(0);
+    }
 }
