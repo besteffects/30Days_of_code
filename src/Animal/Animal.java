e1fb67eaa1ee58196b45edba1bf7b05c4bf7402d
Vitalii <besteffects@gmail.com>
2019-06-21 01:50:58 -0400

    Added some acess modifiers to animal package

--- src/Animal/Animal.java
+++ src/Animal/Animal.java
@@ -1,7 +1,7 @@
 package Animal;
 
 public class Animal {
-    public int age;
+    private int age;
 
     public Animal(int age){
         this.age=age;
@@ -11,17 +11,24 @@ public class Animal {
     public void eat(){
         System.out.println("An animal is eating");
     }
+    public int getAge(){
+        return age;
+    }
 
     public static void main(String[] args) {
         Animal a=new Animal(5);
         Dog d = new Dog();
         Cat c= new Cat();
         d.ruff();
+        System.out.println(d.getAge());
         c.meow();
+        System.out.println(c.getAge());
         a.eat();
         d.eat();
         c.eat();
         d.run();
         c.prence();
+        // What happens if...
+
     }
 }
