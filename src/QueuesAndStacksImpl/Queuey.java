ac4f827d0a82a2e74eff583c3c8f02bfcea85555
Vitalii <besteffects@gmail.com>
2019-06-27 01:24:10 -0400

    Queues and stacks tutorial.Changed imports

--- src/QueuesAndStacksImpl/Queuey.java
+++ src/QueuesAndStacksImpl/Queuey.java
@@ -1,6 +1,6 @@
 package QueuesAndStacksImpl;
 
-import LinkedListImpl.LinkedList;
+import java.util.LinkedList;
 
 public class Queuey {
     LinkedList queue;
@@ -21,14 +21,25 @@ public class Queuey {
 
      //Enqueueing an item
      public void enque(int n){
-        queue.add(n);
+        queue.addLast(n);
+     }
+
+     public int peek(){
+       return (int)queue.get(0);
      }
 
      //Dequeueing an item
-//     public int dequeue(){
-//        return (int) queue.remove(0);
-//     }
+     public int dequeue(){
+        return (int)queue.remove(0);
+     }
 
     public static void main(String[] args) {
+   Queuey numberqueue =new Queuey();
+   numberqueue.enque(5);
+   numberqueue.enque(7);
+   numberqueue.enque(6);
+   System.out.println("First out: " + numberqueue.dequeue());
+        System.out.println("Second out: " + numberqueue.dequeue());
+        System.out.println("Second out: " + numberqueue.dequeue());
     }
 }
