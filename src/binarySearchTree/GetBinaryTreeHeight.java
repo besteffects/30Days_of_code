a3a16fa3272ca923b28c3e483510146e67c4d683
Vitalii <besteffects@gmail.com>
2019-07-04 23:54:08 -0400

    Get binary Tree height

--- src/binarySearchTree/GetBinaryTreeHeight.java
+++ src/binarySearchTree/GetBinaryTreeHeight.java
@@ -1,5 +1,9 @@
 package binarySearchTree;
-
+/*
+The height of a binary search tree is the number of edges between the tree's root and its furthest leaf. You are given a pointer, root,
+pointing to the root of a binary search tree. Complete the getHeight function provided in your editor so that it returns
+the height of the binary search tree.
+ */
 import java.util.*;
 import java.io.*;
 class Node{
@@ -12,9 +16,21 @@ class Node{
 }
 class GetBinaryTreeHeight{
 
-   // public static int getHeight(Node root){
+    public static int getHeight(Node root){
         //Write your code here
-  //  }
+            if (root == null)
+                return -1;
+            else
+            {
+                int left = getHeight(root.left);
+                int right = getHeight(root.right);
+
+                if (left > right)
+                    return (left + 1);
+                else
+                    return (right + 1);
+            }
+        }
 
     public static Node insert(Node root,int data){
         if(root==null){
@@ -41,7 +57,7 @@ class GetBinaryTreeHeight{
             int data=sc.nextInt();
             root=insert(root,data);
         }
-      //  int height=getHeight(root);
-    //    System.out.println(height);
+        int height=getHeight(root);
+        System.out.println(height);
     }
 }
