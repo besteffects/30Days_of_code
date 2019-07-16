03eaacc97ccefbcfbe1a219e8831eac821061ea2
Vitalii <besteffects@gmail.com>
2019-07-16 02:14:39 -0400

    Linked lists. Remove duplicates. Added task description

--- src/linkedListTwo/LinkedListRemoveDuplicates.java
+++ src/linkedListTwo/LinkedListRemoveDuplicates.java
@@ -9,6 +9,21 @@ class Node{
         next=null;
     }
 }
+/*
+A Node class is provided for you in the editor. A Node object has an
+integer data field, data, and a Node instance pointer, next, pointing to another node
+(i.e.: the next node in a list).
+A removeDuplicates function is declared in your editor, which takes a pointer to the head
+node of a linked list as a parameter. Complete removeDuplicates so that it deletes any
+duplicate nodes from the list and returns the head of the updated list.
+Note: The head pointer may be null, indicating that the list is empty.
+Be sure to reset your next pointer when performing deletions to avoid breaking the list.
+Input format:
+The first line contains an integer, N, the number of nodes to be inserted.
+The N subsequent lines each contain an integer describing the data value of a node
+being inserted at the list's tail.
+ */
+
 class LinkedListRemoveDuplicates
 {
 
