5bd38ffd0d2f10251a5035f79429e9e1a8eb4a86
Vitalii <besteffects@gmail.com>
2019-07-20 01:46:40 -0400

    Runtime practice. Prime numbers. Initial commit

---
+++ src/runningTime/PrimeNumbers.java
@@ -0,0 +1,15 @@
+package runningTime;
+import java.io.*;
+import java.util.*;
+
+public class PrimeNumbers {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT.
+        Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        int[] a = new int[t];
+    }
+}
+
