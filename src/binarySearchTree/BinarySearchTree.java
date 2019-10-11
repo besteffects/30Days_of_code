840e7d722ad9d82195dfa81637732d2401df7d8d
Vitalii <besteffects@gmail.com>
2019-10-11 01:25:22 -0400

    Added test case for BinarySearchTree

--- src/binarySearchTree/BinarySearchTree.java
+++ src/binarySearchTree/BinarySearchTree.java
@@ -3,6 +3,9 @@ package binarySearchTree;
 public class BinarySearchTree {
 
     public static void main(String[] args) {
-
+EmptyBST e = new EmptyBST();
+NonemptyBST n = new NonemptyBST(5);
+Testers.checkIsEmpty(e);
+Testers.checkIsEmpty(n);
     }
 }
