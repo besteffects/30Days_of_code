a942ffabb5a4ccff153ebce3b21c832de403e164
Vitalii <besteffects@gmail.com>
2019-10-11 22:52:56 -0400

    26_Nested logic. Calculate the difference in time

---
+++ src/NestedLogic.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+/*
+Your local library needs your help! Given the expected and actual return dates for a library book, create a program that
+calculates the fine (if any). The fee structure is as follows:
+1. If the book is returned on or before the expected return date, no fine will be charged (i.e.: fine=0).
+2. If the book is returned after the expected return day but still within the same calendar month and year
+ as the expected return date, fine=15 Hackos x (number of days late).
+3. If the book is returned after the expected return month but still within the same calendar year as the
+ expected return date, the fine=500Hackos x (the number of months late).
+4. If the book is returned after the calendar year in which it was expected, there is a fixed fine of
+10000Hackos.
+Input format:
+The first line contains 3 space-separated integers denoting the respective day, month,
+and year on which the book was actually returned.
+The second line contains 3 space-separated integers denoting the respective day, month, and year on which the book was expected to be returned (due date).
+ */
+
+public class NestedLogic {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
+
