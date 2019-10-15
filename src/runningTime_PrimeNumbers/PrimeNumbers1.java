8843caa226dcd7bd875bb3ab0ea49655e78e24dc
Vitalii <besteffects@gmail.com>
2019-10-15 19:10:42 -0400

    27_Testing part2, Added another resolution for Prime numbers

--- src/runningTime_PrimeNumbers/PrimeNumbers1.java
+++ src/runningTime_PrimeNumbers/PrimeNumbers1.java
@@ -2,13 +2,14 @@ package runningTime_PrimeNumbers;
 
 import java.util.Scanner;
 
-public class PrimeNumbers_toFix {
+public class PrimeNumbers1 {
     public static void main(String[] args) {
         // Enter your code here. Read input from STDIN. Print output to STDOUT.
-        int flag = 0;
+
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(); //number of test cases
         for (int i = 0; i < a; i++) {
+            int flag = 0;
             long n = sc.nextLong();// numbers we test
             if (n == 0 || n == 1) {
                 System.out.println("Not prime");
