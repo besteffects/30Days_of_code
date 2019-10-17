1a993c5c723bbd571969559be55f42f721734fdd
Vitalii <besteffects@gmail.com>
2019-10-17 02:25:01 -0400

    Unit testing. Part2. Added get methods

--- src/unitTesting/UnitTestingTask.java
+++ src/unitTesting/UnitTestingTask.java
@@ -14,18 +14,19 @@ with expectations. Your task is to implement 3 classes that will produce test da
 for the testing functions. More specifically: function get_array() in TestDataEmptyArray class and
 functions get_array() and get_expected_result() in classes TestDataUniqueValues and
 TestDataExactlyTwoDifferentMinimums following the below specifications:
-    -get_array() method in class TestDataEmptyArray has to return an empty array.
-    -get_array() method in class TestDataUniqueValues has to return an array of size at least 2 with
+    -1 get_array() method in class TestDataEmptyArray has to return an empty array.
+    -2 get_array() method in class TestDataUniqueValues has to return an array of size at least 2 with
     all unique elements, while method get_expected_result() of this class has to return the expected
     minimum value index for this array.
-    -get_array() method in class TestDataExactlyTwoDifferentMinimums has to return an array where
+    -3 get_array() method in class TestDataExactlyTwoDifferentMinimums has to return an array where
     there are exactly two different minimum values, while method get_expected_result() of this
     class has to return the expected minimum value index for this array.
 Take a look at the code template to see the exact implementation of functions that your colleagues already implemented.
  */
 package unitTesting;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
 
 public class UnitTestingTask {
 
@@ -42,31 +43,95 @@ public class UnitTestingTask {
         return min_idx;
     }
 
- /*   static class TestDataEmptyArray {
+    static class TestDataEmptyArray {
         public static int[] get_array() {
             // complete this function
+            int[] array = {};
+            return array;
         }
     }
 
     static class TestDataUniqueValues {
+        static int[] array = {1, 2, 3, 4};
+
         public static int[] get_array() {
             // complete this function
+            return array;
         }
 
         public static int get_expected_result() {
             // complete this function
+            return 0;
         }
     }
 
     static class TestDataExactlyTwoDifferentMinimums {
+        static int[]array={1,1,2,3,4};
         public static int[] get_array() {
             // complete this function
+            return array;
         }
 
         public static int get_expected_result() {
             // complete this function
+            return 0;
         }
-
-  */
     }
 
+    public static void TestWithEmptyArray() {
+        try {
+            int[] seq = TestDataEmptyArray.get_array();
+            int result = minimum_index(seq);
+        } catch (IllegalArgumentException e) {
+            return;
+        }
+        throw new AssertionError("Exception wasn't thrown as expected");
+    }
+
+    public static void TestWithUniqueValues() {
+        int[] seq = TestDataUniqueValues.get_array();
+        if (seq.length < 2) {
+            throw new AssertionError("less than 2 elements in the array");
+        }
+
+        Integer[] tmp = new Integer[seq.length];
+        for (int i = 0; i < seq.length; ++i) {
+            tmp[i] = Integer.valueOf(seq[i]);
+        }
+        if (!( ( new LinkedHashSet<Integer>(Arrays.asList(tmp)) ).size() == seq.length )) {
+            throw new AssertionError("not all values are unique");
+        }
+
+        int expected_result = TestDataUniqueValues.get_expected_result();
+        int result = minimum_index(seq);
+        if (result != expected_result) {
+            throw new AssertionError("result is different than the expected result");
+        }
+    }
+
+    public static void TestWithExactlyTwoDifferentMinimums() {
+        int[] seq = TestDataExactlyTwoDifferentMinimums.get_array();
+        if (seq.length < 2) {
+            throw new AssertionError("less than 2 elements in the array");
+        }
+
+        int[] tmp = seq.clone();
+        Arrays.sort(tmp);
+        if (!( tmp[0] == tmp[1] && ( tmp.length == 2 || tmp[1] < tmp[2] ) )) {
+            throw new AssertionError("there are not exactly two minimums in the array");
+        }
+
+        int expected_result = TestDataExactlyTwoDifferentMinimums.get_expected_result();
+        int result = minimum_index(seq);
+        if (result != expected_result) {
+            throw new AssertionError("result is different than the expected result");
+        }
+    }
+
+    public static void main(String[] args) {
+        TestWithEmptyArray();
+        TestWithUniqueValues();
+        TestWithExactlyTwoDifferentMinimums();
+        System.out.println("OK");
+    }
+}
